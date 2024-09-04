4883534c22fd80be18da89d437d78865c5f83a7e
kengiiGMs <kaikiaoto22@gmail.com>
2024-09-03 23:52:02 -0300

    feat: Implemented front-end components for editing animals, and integrated them with the back-end

--- src/pages/animal.jsx
+++ src/pages/animal.jsx
@@ -9,10 +9,54 @@ import { Toast } from 'primereact/toast'
 import { Tag } from 'primereact/tag';
 import { classNames } from 'primereact/utils';
 import { DataView } from 'primereact/dataview';
+import { Dialog } from 'primereact/dialog';
+import { InputText } from 'primereact/inputtext';
+import { Dropdown } from "primereact/dropdown";
 
 import { useEffect, useState, useRef } from "react";
 
 export default function Animal() {
+    /* Coleta de dados do animal */
+    const [animalEdit, setAnimalEdit] = useState({
+        nome: '',
+        sexo: '',
+        data: '',
+        tipo: '',
+        cor: '',
+        porte: '',
+        castrado: '',
+        adocao: '',
+        raca: ''
+    });
+
+    const sexoList = [
+        { label: 'Macho', value: 'Macho' },
+        { label: 'Fêmea', value: 'Fêmea' }
+    ];
+
+    const porteList = [
+        { label: 'Pequeno', value: 'Pequeno' },
+        { label: 'Medio', value: 'Medio' },
+        { label: 'Grande', value: 'Grande' }
+    ];
+
+    const castradoList = [
+        { label: 'Sim', value: 'Sim' },
+        { label: 'Não', value: 'Não' }
+    ];
+
+    const adocaoList = [
+        { label: 'Sim', value: 'Sim' },
+        { label: 'Não', value: 'Não' }
+    ];
+
+    const tipoList = [
+        { label: 'Cachorro', value: 'Cachorro' },
+        { label: 'Gato', value: 'Gato' },
+        { label: 'Ave', value: 'Ave' }
+    ];
+
+    /* Coleta de dados do animal */
 
     /* Btn navigate to CreateAnimal */
     const navigate = useNavigate();
@@ -50,10 +94,63 @@ export default function Animal() {
                         </div>
                         <Toast ref={toast} />
                         <ConfirmPopup target={buttonEl} visible={visibleDeleteModal} onHide={() => setVisibleDeleteModal(false)} accept={accept} reject={reject} message='Você deseja excluir esse Animal?' />
+                        <Dialog header="Header" visible={visibleEdit} style={{ width: '50vw' }} onHide={() => { if (!visibleEdit) return; setVisibleEdit(false); }} >
+                            <p className="m-0">
+                                <form onSubmit={handleSubmit} className='formgrid grid m-4'>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Dropdown name="tipo" value={animalEdit.tipo} onChange={(e) => handleDropdownChange(e, 'tipo')} options={tipoList}
+                                            placeholder="Tipo" className="w-full" required />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Dropdown value={animalEdit.sexo} onChange={(e) => handleDropdownChange(e, 'sexo')} options={sexoList}
+                                            placeholder="Selecione o Sexo" className="w-full" required />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Dropdown value={animalEdit.porte} onChange={(e) => handleDropdownChange(e, 'porte')} options={porteList}
+                                            placeholder="Selecione o Porte" className="w-full" required />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Dropdown value={animalEdit.castrado} onChange={(e) => handleDropdownChange(e, 'castrado')} options={castradoList}
+                                            placeholder="Castrado ?" className="w-full" required />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Dropdown value={animalEdit.adocao} onChange={(e) => handleDropdownChange(e, 'adocao')} options={adocaoList}
+                                            placeholder="Para adoção ?" className="w-full" required />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <InputText keyfilter={/^[a-zA-ZçÇéíóúÁÉÍÓÚâêîôûÂÊÎÔÛãõÃÕàÀ\s]*$/} placeholder='Nome' name="nome" value={animalEdit.nome} onChange={handleChange} required className='w-full' />
+
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <InputText keyfilter={/^[a-zA-ZçÇéíóúÁÉÍÓÚâêîôûÂÊÎÔÛãõÃÕàÀ\s]*$/} placeholder='Raça' name="raca" value={animalEdit.raca} onChange={handleChange} required className='w-full' />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <InputText type='date' placeholder='Data' name="data" value={animalEdit.data} onChange={handleChange} required className='w-full' />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <InputText keyfilter={/^[a-zA-ZçÇéíóúÁÉÍÓÚâêîôûÂÊÎÔÛãõÃÕàÀ\s]*$/} placeholder='Cor' name="cor" value={animalEdit.cor} onChange={handleChange} required className='w-full' />
+                                    </div>
+
+                                    <div className='field col-12 md:col-6'>
+                                        <Button type='submit' label='Cadastrar' severity='help' text raised className='w-full' />
+                                    </div>
+
+                                </form>
+                            </p>
+                        </Dialog>
                         <div className="flex sm:flex-column align-items-center sm:align-items-end gap-3 sm:gap-2">
 
                             <Button onClick={(e) => handleDelete(animal.id, e.currentTarget)} icon='pi pi-times' className="p-button-rounded" />
-                            <Button icon="pi pi-pencil" className="p-button-rounded" ></Button>
+                            <Button onClick={() => handleEdit(animal.id)} icon="pi pi-pencil" className="p-button-rounded" ></Button>
                             <Button icon="pi pi-heart-fill" className="p-button-rounded" ></Button>
 
                         </div>
@@ -119,6 +216,85 @@ export default function Animal() {
         setVisibleDeleteModal(false);
     };
     /* Delete animal */
+
+    /* Edit animal */
+    const [visibleEdit, setVisibleEdit] = useState(false);
+    const [animalIdToEdit, setAnimalIdToEdit] = useState(null);
+
+    /* Busca dados do animal através do id */
+    useEffect(() => {
+        if (animalIdToEdit) {
+            fetch(`http://localhost:8080/animais/${animalIdToEdit}`)
+                .then(response => response.json())
+                .then(data => setAnimalEdit(data))
+                .catch(error => console.error('Erro ao buscar animal:', error));
+        }
+    }, [animalIdToEdit]);
+    /* Busca dados do animal através do id */
+
+
+    /* Abre a modal e armazena o id para editar */
+    const handleEdit = async (id) => {
+        setAnimalIdToEdit(id);
+        setVisibleEdit(true)
+    };
+    /* Abre a modal e armazena o id para editar */
+
+    /* De acordo com as alterações do input, ele salva a alteração */
+
+    /* InputText */
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setAnimalEdit((prevAnimalEdit) => ({
+            ...prevAnimalEdit,
+            [name]: value
+        }));
+    };
+    /* InputText */
+
+    /* DropDown */
+    const handleDropdownChange = (e, name) => {
+        setAnimalEdit((prevAnimalEdit) => ({
+            ...prevAnimalEdit,
+            [name]: e.value
+        }));
+    };
+    /* DropDown */
+
+    /* De acordo com as alterações do input, ele salva a alteração */
+
+    /* Estrutura para salvar alterações + atualiza a lista de animais */
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch(`http://localhost:8080/animais/${animalIdToEdit}`, {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(animalEdit),
+            });
+
+            if (response.ok) {
+                toast.current.show({ severity: 'info', summary: 'Operação Realizada !', detail: 'Animal Editado com sucesso', life: 3000 });
+                const updateResponse1 = await fetch('http://localhost:8080/animais');
+                if (updateResponse1.ok) {
+                    const updateAnimais = await updateResponse1.json();
+                    setAnimais(updateAnimais);
+                } else {
+                    toast.current.show({ severity: 'error', summary: 'Erro !', detail: 'Erro ao recarregar lista animal', life: 3000 });
+                }
+            } else {
+                console.error('Erro ao atualizar animal:', response.statusText);
+            }
+        } catch (error) {
+            console.error('Erro na requisição:', error);
+        }
+    };
+
+    /* Estrutura para salvar alterações + atualiza a lista de animais */
+    /* Edit animal */
 
     return (
         <>
