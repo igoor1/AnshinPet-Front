fc722f9d844c0a25c9c2d42891720907bfb9ce92
kengiiGMs <kaikiaoto22@gmail.com>
2024-09-24 11:09:33 -0300

    refator: Redesigned vacina page layout and styling

--- src/components/vacina/CreateVacina.jsx
+++ src/components/vacina/CreateVacina.jsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { useCreateVacina } from '../../hooks/vacina/useCreateVacina';
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { InputText } from "primereact/inputtext";
 
-const CreateVacina = ({ onClose, onRefresh, onShowToast }) => {
+import { Loading } from '../../components/loading/loading';
+
+const CreateVacina = ({ onClose, onRefresh, onShowToast, visibleCreate }) => {
     const { createVacina, loading, error } = useCreateVacina();
     const [formValues, setFormValues] = useState({ nome: '', fabricante: '' });
+    const formRef = useRef(null);
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -17,41 +23,54 @@ const CreateVacina = ({ onClose, onRefresh, onShowToast }) => {
             onShowToast('success', 'Sucesso', 'Vacina cadastrada com sucesso!');
             setTimeout(onRefresh, 1500);
             setTimeout(onClose, 1500);
+            onClose();
         } catch (err) {
             onShowToast('error', 'Erro', 'Falha ao cadastrar a Vacina.');
         }
     };
 
-    if (loading) return <p>Carregando...</p>;
+    const footerContent = (
+        <div>
+            <Button label="Cancelar" icon="pi pi-times" severity="danger" onClick={onClose} className="p-button-text" />
+            <Button label="Cadastrar" icon="pi pi-check" severity="success" onClick={() => formRef.current.requestSubmit()} autoFocus />
+        </div>
+    );
+
     if (error) return <p>{error}</p>;
 
     return (
         <>
-            <form onSubmit={handleSubmit}>
-                <h2>Cadastrar Vacina</h2>
-                <div>
-                    <label>Nome:</label>
-                    <input
-                        type="text"
-                        name="nome"
-                        value={formValues.nome || ''}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Fabricante:</label>
-                    <input
-                        type="text"
-                        name="fabricante"
-                        value={formValues.fabricante || ''}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Cadastrar</button>
-                <button type="button" onClick={onClose}>Cancelar</button>
-            </form>
+            <Dialog header="Cadastrar" visible={visibleCreate} style={{ width: '50vw' }} onHide={onClose} footer={footerContent}>
+                {loading ? (
+                    <Loading height="50vh" />
+
+                ) : (
+                    <form onSubmit={handleSubmit} ref={formRef}>
+                        <div>
+                            <div className="flex justify-content-center mt-2">
+                                <div className="flex flex-column gap-2 w-full">
+                                    <label htmlFor="nome">Nome</label>
+                                    <InputText id="nome" name="nome"
+                                        value={formValues.nome || ''}
+                                        onChange={handleChange}
+                                        required style={{ width: '100%' }} />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex justify-content-center mt-2">
+                                <div className="flex flex-column gap-2 w-full">
+                                    <label htmlFor="fabricante">Fabricante</label>
+                                    <InputText id="fabricante" name="fabricante"
+                                        value={formValues.fabricante || ''}
+                                        onChange={handleChange}
+                                        required style={{ width: '100%' }} />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                )}
+            </Dialog>
         </>
     );
 };
--- src/components/vacina/EditVacina.jsx
+++ src/components/vacina/EditVacina.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect, useRef } from 'react';
 
 import { Toast } from "primereact/toast";
+import { Dialog } from 'primereact/dialog';
+import { Button } from 'primereact/button';
+import { InputText } from "primereact/inputtext";
+
+import { Loading } from '../../components/loading/loading';
 
 import { useEditVacina } from '../../hooks/vacina/useEditVacina';
 
-const EditVacina = ({ vacinaId, onClose, onRefresh, onShowToast }) => {
+const EditVacina = ({ vacinaId, onClose, onRefresh, onShowToast, visibleEdit }) => {
     const { vacina, loading, error, updateVacina } = useEditVacina(vacinaId);
     const [formValues, setFormValues] = useState({});
     const toast = useRef(null);
+    const formRef = useRef(null);
 
     useEffect(() => {
         if (vacina) {
@@ -27,42 +33,55 @@ const EditVacina = ({ vacinaId, onClose, onRefresh, onShowToast }) => {
             onShowToast('success', 'Sucesso', 'Vacina editada com sucesso!');
             setTimeout(onRefresh, 1500);
             setTimeout(onClose, 1500);
+            onClose();
         } catch {
             onShowToast('error', 'Erro', 'Falha ao editar a Vacina.');
         }
     };
 
-    if (loading) return <p>Carregando...</p>;
+    const footerContent = (
+        <div>
+            <Button label="Cancelar" icon="pi pi-times" severity="danger" onClick={onClose} className="p-button-text" />
+            <Button label="Editar" icon="pi pi-check" severity="success" onClick={() => formRef.current.requestSubmit()} autoFocus />
+        </div>
+    );
+
     if (error) return <p>{error}</p>;
 
     return (
         <>
             <Toast ref={toast} />
-            <form onSubmit={handleSubmit}>
-                <h2>Editar Vacina</h2>
-                <div>
-                    <label>Nome:</label>
-                    <input
-                        type="text"
-                        name="nome"
-                        value={formValues.nome || ''}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <div>
-                    <label>Fabricante:</label>
-                    <input
-                        type="text"
-                        name="fabricante"
-                        value={formValues.fabricante || ''}
-                        onChange={handleChange}
-                        required
-                    />
-                </div>
-                <button type="submit">Salvar</button>
-                <button type="button" onClick={onClose}>Cancelar</button>
-            </form>
+            <Dialog header="Editar" visible={visibleEdit} style={{ width: '50vw' }} onHide={onClose} footer={footerContent}>
+                {loading ? (
+                    <Loading height="50vh" />
+
+                ) : (
+                    <form onSubmit={handleSubmit} ref={formRef}>
+                        <div>
+                            <div className="flex justify-content-center mt-2">
+                                <div className="flex flex-column gap-2 w-full">
+                                    <label htmlFor="nome">Nome</label>
+                                    <InputText id="nome" name="nome"
+                                        value={formValues.nome || ''}
+                                        onChange={handleChange}
+                                        required style={{ width: '100%' }} />
+                                </div>
+                            </div>
+                        </div>
+                        <div>
+                            <div className="flex justify-content-center mt-2">
+                                <div className="flex flex-column gap-2 w-full">
+                                    <label htmlFor="Fabricante">Fabricante</label>
+                                    <InputText id="fabricante" name="fabricante"
+                                        value={formValues.fabricante || ''}
+                                        onChange={handleChange}
+                                        required style={{ width: '100%' }} />
+                                </div>
+                            </div>
+                        </div>
+                    </form>
+                )}
+            </Dialog>
         </>
     );
 };
