417eeb063d7a06451da886e7c9b26b948104e772
kengiiGMs <kaikiaoto22@gmail.com>
2024-10-03 19:34:43 -0300

    feat: Add bootstrap to project

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,9 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom"
 
-import { Animal } from './pages/animal/animal'
-import { CreateAnimal } from './pages/animal/createAnimal'
-import { Medicals } from "./pages/animal/medicals"
-import { Login } from "./pages/login/login"
-import { Dashboard } from "./pages/dashboard/dashboard"
-import { Doacao } from "./pages/doacao/doacao"
+import Animal from './pages/animal/animal'
 
-import { Doenca } from './pages/doenca/doenca'
-import { Vacina } from './pages/vacina/vacina'
-
-import 'primereact/resources/themes/lara-light-indigo/theme.css';  // Tema
-import 'primereact/resources/primereact.min.css';           // PrimeReact
-import 'primeicons/primeicons.css';                         // Icons
-import 'primeflex/primeflex.css';                           //PrimeFlex
+import './scss/styles.scss';
+import 'bootstrap-icons/font/bootstrap-icons.css';
 
 
 export default function App() {
@@ -22,13 +12,6 @@ export default function App() {
     <Router>
       <Routes>
         <Route path="/animal" element={<Animal />} />
-        <Route path="/animal/create" element={<CreateAnimal />} />
-        <Route path="/animal/medicals/:animalId" element={<Medicals />} />
-        <Route path="/doenca" element={<Doenca />} />
-        <Route path="/vacina" element={<Vacina />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/doacao" element={<Doacao />} />
       </Routes>
     </Router>
   )
