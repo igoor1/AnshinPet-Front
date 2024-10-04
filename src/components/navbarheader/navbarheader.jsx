b92822f0681112fd3c48ca11a8a0bdcada807a02
kengiiGMs <kaikiaoto22@gmail.com>
2024-10-04 09:41:07 -0300

    refactor: Update navbar styles to improve elements sizing and positioning

--- src/components/navbarheader/navbarheader.jsx
+++ src/components/navbarheader/navbarheader.jsx
@@ -5,30 +5,32 @@ import Logo from '../../assets/logoWhite.svg';
 
 const NavbarHeader = () => {
     return (
-        <Navbar collapseOnSelect expand="lg" bg='primary' className='my-navbar-theme' data-bs-theme="dark">
-            <Container>
-                <Navbar.Brand><Image src={Logo} alt="Logo" style={{ width: '30px', height: '30px' }} roundedCircle /></Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" bg='primary' className='my-navbar-theme p-2' data-bs-theme="dark">
+            <Container fluid>
+                <Navbar.Brand>
+                    <Image src={Logo} alt="Logo" roundedCircle className='logo' />
+                </Navbar.Brand>
                 <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto gap-2">
-                        <Nav.Link href="/dashboard"><i class="bi bi-house-door"></i> Home</Nav.Link>
-                        <Nav.Link href="/animais"><i class="bi bi-mailbox"></i> Animais</Nav.Link>
-                        <NavDropdown title={<span><i class="bi bi-heart-pulse"></i> Cuidados Médicos</span>} id="collapsible-nav-dropdown">
-                            <NavDropdown.Item href="/doencas"><i class="bi bi-backpack4"></i> Doenças</NavDropdown.Item>
-                            <NavDropdown.Item href="/vacinas"><i class="bi bi-eyedropper"></i> Vacinas</NavDropdown.Item>
+                        <Nav.Link href="/dashboard"><i className="bi bi-house-door"></i> Home</Nav.Link>
+                        <Nav.Link href="/animais"><i className="bi bi-mailbox"></i> Animais</Nav.Link>
+                        <NavDropdown title={<span><i className="bi bi-heart-pulse"></i> Cuidados Médicos</span>} id="collapsible-nav-dropdown">
+                            <NavDropdown.Item href="/doencas"><i className="bi bi-backpack4"></i> Doenças</NavDropdown.Item>
+                            <NavDropdown.Item href="/vacinas"><i className="bi bi-eyedropper"></i> Vacinas</NavDropdown.Item>
                         </NavDropdown>
-                        <Nav.Link href="/doacoes"><i class="bi bi-archive"></i> Doações</Nav.Link>
-                        <Nav.Link href="/cuidadores"><i class="bi bi-people"></i> Cuidadores</Nav.Link>
+                        <Nav.Link href="/doacoes"><i className="bi bi-archive"></i> Doações</Nav.Link>
+                        <Nav.Link href="/cuidadores"><i className="bi bi-people"></i> Cuidadores</Nav.Link>
                     </Nav>
                     <Nav>
                         <DropdownButton
                             align={{ lg: 'end' }}
                             id="dropdown-menu-align-responsive-1"
-                            title={<Image src="https://avatars.githubusercontent.com/u/89944667?v=4" alt="Logo" style={{ width: '30px', height: '30px' }} roundedCircle />}
+                            title={<Image src="https://avatars.githubusercontent.com/u/89944667?v=4" alt="Foto do Perfil" className='ImagemUsuario' roundedCircle />}
                         >
-                            <Dropdown.Item href="/configuracoes"><i class="bi bi-gear"></i> Configurações</Dropdown.Item>
+                            <Dropdown.Item href="/configuracoes"><i className="bi bi-gear"></i> Configurações</Dropdown.Item>
                             <NavDropdown.Divider />
-                            <Dropdown.Item href="/login"><i class="bi bi-arrow-left-square"></i> Logout</Dropdown.Item>
+                            <Dropdown.Item href="/login"><i className="bi bi-arrow-left-square"></i> Logout</Dropdown.Item>
                         </DropdownButton>
                     </Nav>
                 </Navbar.Collapse>
