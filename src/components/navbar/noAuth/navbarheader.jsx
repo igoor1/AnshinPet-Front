bc32905db44929153912d1e166fd57efb232248c
kengiiGMs <kaikiaoto22@gmail.com>
2024-10-26 16:35:03 -0300

    refactor: Update the design of the home page and update the colors in the project

--- src/components/navbar/noAuth/navbarheader.jsx
+++ src/components/navbar/noAuth/navbarheader.jsx
@@ -1,17 +1,19 @@
 import { Container, Nav, Navbar, Image } from 'react-bootstrap';
 import './navbarheader.scss'
 
-import Logo from '../../../assets/logoWhite.svg';
+import Logo from '../../../assets/logoMinimal.svg';
 
 const NavbarHeader = () => {
 
     return (
-        <Navbar collapseOnSelect expand="lg" bg='primary' className='my-navbar-theme p-2' data-bs-theme="dark">
+        <Navbar collapseOnSelect expand="lg" className='my-navbar-theme p-2' data-bs-theme="primary">
             <Container fluid>
                 <Navbar.Brand>
                     <Image src={Logo} alt="Logo" roundedCircle className='logo' />
                 </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Toggle aria-controls="responsive-navbar-nav">
+                    <i class="bi bi-list"></i>
+                </Navbar.Toggle>
                 <Navbar.Collapse id="responsive-navbar-nav">
                     <Nav className="me-auto gap-2">
                         <Nav.Link href="/"><i className="bi bi-house-door"></i> Home</Nav.Link>
@@ -19,7 +21,6 @@ const NavbarHeader = () => {
                         <Nav.Link href="/doar"><i className="bi bi-box2-heart"></i> Doar</Nav.Link>
                         <Nav.Link href="/sobre"><i className="bi bi-people"></i> Sobre nós</Nav.Link>
                     </Nav>
-
                 </Navbar.Collapse>
             </Container>
         </Navbar>
