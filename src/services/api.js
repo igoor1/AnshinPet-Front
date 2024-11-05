30ef2c0bf728e49973ca0f0e549e0503b4355d78
kengiiGMs <kaikiaoto22@gmail.com>
2024-11-05 20:38:37 -0300

    fix: Correct to script on package.json

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const apiUrl = import.meta.env.REACT_APP_API_URL;
+const apiUrl = import.meta.env.VITE_API_URL;
+console.log(import.meta.env.VITE_API_URL);
 
 export const api = axios.create({
     baseURL: apiUrl,
